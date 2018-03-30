f00a9b4c1732d061555deb969e80812ed20ac355
Michael A Nowiszewski <micski@gmail.com>
2018-03-30 10:06:12 +0000

    catScratchFeverloop!

---
+++ catScratchFever.c
@@ -0,0 +1,12 @@
+#
+include < stdio.h >
+  int main() {
+    int number = 20;
+    while (number > 10) {
+      printf(“the number is % d”, number);
+      number--;
+    }
+    return 0;
+  }
+  
+  //print num , if less than 10 decrement divisor
